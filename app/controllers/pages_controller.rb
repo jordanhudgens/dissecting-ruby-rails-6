d3a58427f1616bf32f377a4fc497e75255506107
jordanhudgens <jordan@devcamp.com>
2020-08-20 09:55:09 -0700

    Authenticate user guard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:admin_dashboard]
+
   def home
+  end
+
+  def admin_dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  get :admin_dashboard, to: "pages#admin_dashboard"
   root to: "pages#home"
 end
